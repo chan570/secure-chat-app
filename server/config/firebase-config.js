4f812a8cf95db07a04c5812527c93495c28ab6c1
Chanpreet Kaur <kaurchan2005@gmail.com>
2026-04-24 01:01:38 +0530

    remove serviceAccountKey dependency

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,15 +1,16 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-// Read Firebase config from environment variable
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT is missing");
+}
+
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-// Initialize Firebase Admin
 const app = initializeApp({
   credential: cert(serviceAccount),
 });
 
-// Get Auth instance
 const auth = getAuth(app);
 
 export default auth;
